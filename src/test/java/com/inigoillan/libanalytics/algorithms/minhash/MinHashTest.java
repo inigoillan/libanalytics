c184a3967e31823ecaf49f3184a797e8752a0f0e
ignacius <inigo.illan@gmail.com>
2015-08-04 17:46:13 +0100

    Added MinHash tests

---
+++ src/test/java/com/inigoillan/libanalytics/algorithms/minhash/MinHashTest.java
@@ -0,0 +1,100 @@
+package com.inigoillan.libanalytics.algorithms.minhash;
+
+import com.inigoillan.libanalytics.algorithms.hashers.Hash;
+import com.inigoillan.libanalytics.algorithms.hashers.Hash32Bits;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class MinHashTest {
+
+    //region EstimateJaccardIndex tests
+
+    @Test
+    public void EstimateJaccardIndex_FirstMinHashSizeIs0_ReturnZero() throws Exception {
+        // Arrange
+        MinHash<Hash> minHash = buildMinHash(10, new int[0]);
+        MinHash<Hash> minHash2 = buildMinHash(10, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+
+        // Act
+        float result = minHash.estimateJaccardIndex(minHash2);
+
+        // Assert
+        assertEquals(0.0f, result, 0.0f);
+    }
+
+    @Test
+    public void EstimateJaccardIndex_SecondMinHashSizeIs0_ReturnZero() throws Exception {
+        // Arrange
+        MinHash<Hash> minHash = buildMinHash(10, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+        MinHash<Hash> minHash2 = buildMinHash(10, new int[0]);
+
+        // Act
+        float result = minHash.estimateJaccardIndex(minHash2);
+
+        // Assert
+        assertEquals(0.0f, result, 0.0f);
+    }
+
+
+    @Test
+    public void EstimateJaccardIndex_0ElementsIncommon_JaccardIndexIs0() throws Exception {
+        // Arrange
+        MinHash<Hash> minHash = buildMinHash(10, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+        MinHash<Hash> minHash2 = buildMinHash(10, new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19});
+
+        // Act
+        float result = minHash.estimateJaccardIndex(minHash2);
+
+        // Assert
+        assertEquals(0.0f, result, 0.0f);
+    }
+
+    @Test
+    public void EstimateJaccardIndex_5OutOf10ElementsIncommon_JaccardIndexIsZeroPointFive() throws Exception {
+        // Arrange
+        MinHash<Hash> minHash = buildMinHash(10, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+        MinHash<Hash> minHash2 = buildMinHash(10, new int[]{5, 6, 7, 8, 9, 10, 11, 12, 13, 14});
+
+        // Act
+        float result = minHash.estimateJaccardIndex(minHash2);
+
+        // Assert
+        assertEquals(0.5f, result, 0.0f);
+    }
+
+    @Test
+    public void EstimateJaccardIndex_DifferentSizedMinHash_JaccardIndexIsZeroPointFive() throws Exception {
+        // Arrange
+        MinHash<Hash> minHash = buildMinHash(10, new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+        MinHash<Hash> minHash2 = buildMinHash(20, new int[]{5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25});
+
+        // Act
+        float result = minHash.estimateJaccardIndex(minHash2);
+
+        // Assert
+        assertEquals(0.5f, result, 0.0f);
+    }
+
+    //endregion
+
+
+    //region Helper methods
+
+    private MinHash<Hash> buildMinHash(int size, int[] hashes) {
+        MinHash<Hash> minHash = new MinHash<>(size);
+
+        for(int i = 0; i < hashes.length; i++) {
+            minHash.addHashed(hash(hashes[i]));
+        }
+
+        return minHash;
+    }
+
+    private Hash hash(int hash) {
+        return new Hash32Bits(hash);
+    }
+
+    //endregion
+}
