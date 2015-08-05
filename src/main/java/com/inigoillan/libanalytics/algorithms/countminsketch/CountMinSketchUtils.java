bf2dad70bef42943ee6b4ae9de0cfa3820ac2399
Inigo Illan <inigo.illan@gmail.com>
2015-08-05 20:34:01 +0100

    Added CountMinSketch utils class and test

---
+++ src/main/java/com/inigoillan/libanalytics/algorithms/countminsketch/CountMinSketchUtils.java
@@ -0,0 +1,56 @@
+package com.inigoillan.libanalytics.algorithms.countminsketch;
+
+import com.inigoillan.libanalytics.hash.Divisible;
+
+import java.util.Arrays;
+
+/**
+ * @author <a href="mailto:inigo.illan@gmail.com">Inigo Illan</a>
+ * @since 1.0
+ */
+public class CountMinSketchUtils {
+
+    //region Estimation Error
+
+    /**
+     * Estimation error for the point query
+     *
+     * @param sketch
+     * @return
+     *
+     * @see CountMinSketch#estimatePointQuery(Divisible[])
+     */
+    public static float getEstimationError(CountMinSketch sketch) {
+        long[] row0 = (long[]) sketch.getSketch().get(0);
+
+        long numEvents = registeredEvents(row0);
+        int width = sketch.getNumCols();
+
+        return 2.0f * numEvents / width;
+    }
+
+    private static long registeredEvents(long[] row) {
+        return Arrays.stream(row).sum();
+    }
+
+    //endregion
+
+
+    //region Probability Error
+
+    /**
+     * Probability error for the point query
+     *
+     * @param sketch
+     * @return
+     *
+     * @see CountMinSketch#estimatePointQuery(Divisible[])
+     */
+    public static float getProbabilityError(CountMinSketch sketch) {
+        int sketchHeight = sketch.getNumRows();
+
+        return 1.0f - (1.0f / (float) Math.pow(2, sketchHeight));
+    }
+
+    //endregion
+}
---
+++ src/test/java/com/inigoillan/libanalytics/algorithms/countminsketch/CountMinSketchUtilsTest.java
@@ -0,0 +1,79 @@
+package com.inigoillan.libanalytics.algorithms.countminsketch;
+
+import com.google.common.collect.Lists;
+import com.inigoillan.libanalytics.hash.Divisible;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author <a href="mailto:inigo.illan@gmail.com">Inigo Illan</a>
+ * @since 1.0
+ */
+public class CountMinSketchUtilsTest {
+
+    //region GetEstimationError tests
+
+    @Test
+    public void GetEstimationError_GivenCountMinSketch_Return20() {
+        // Arrange
+        CountMinSketch<Divisible> sketch = mock(CountMinSketch.class);
+        when(sketch.getSketch()).thenReturn(Lists.newArrayList(new long[]{10L, 10L, 10L}));
+        when(sketch.getNumCols()).thenReturn(3);
+
+        // Act
+        float result = CountMinSketchUtils.getEstimationError(sketch);
+
+        // Assert
+        assertEquals(20.0f, result, 0.0f);
+    }
+
+    @Test
+    public void GetEstimationError_GivenCountMinSketch0Elements_Return0() {
+        // Arrange
+        CountMinSketch<Divisible> sketch = mock(CountMinSketch.class);
+        when(sketch.getSketch()).thenReturn(Lists.newArrayList(new long[]{0L, 0L, 0L}));
+        when(sketch.getNumCols()).thenReturn(3);
+
+        // Act
+        float result = CountMinSketchUtils.getEstimationError(sketch);
+
+        // Assert
+        assertEquals(0.0f, result, 0.0f);
+    }
+
+    //endregion
+
+    //region GetProbabilityError tests
+
+    @Test
+    public void GetProbabilityError_GivenCountMinSketch_ReturnPoint999() {
+        // Arrange
+        CountMinSketch<Divisible> sketch = mock(CountMinSketch.class);
+        when(sketch.getNumRows()).thenReturn(10);
+
+        // Act
+        float result = CountMinSketchUtils.getProbabilityError(sketch);
+
+        // Assert
+        assertEquals(0.999f, result, 0.0001f);
+    }
+
+    @Test
+    public void GetProbabilityError_GivenCountMinSketch0Elements_Return0() {
+        // Arrange
+        CountMinSketch<Divisible> sketch = mock(CountMinSketch.class);
+        when(sketch.getNumRows()).thenReturn(0);
+
+        // Act
+        float result = CountMinSketchUtils.getProbabilityError(sketch);
+
+        // Assert
+        assertEquals(0.0f, result, 0.0f);
+    }
+
+    //endregion
+
+}
